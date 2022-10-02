25595d04edb692880336f8e2bf68ad7505bfbc64
Cristiano <cristianosampaio16@gmail.com>
2022-10-02 10:13:49 -0300

    add one more exercice

--- origamid/react-for-beginners/appreact/src/Exe1.js
+++ origamid/react-for-beginners/appreact/src/Exe1.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
+// const luana = {
+//   cliente: 'Luana',
+//   idade: 27,
+//   compras: [
+//     { nome: 'Notebook', preco: 'R$ 2500' },
+//     { nome: 'Geladeira', preco: 'R$ 3000' },
+//     { nome: 'Smartphone', preco: 'R$ 1500' },
+//   ],
+//   ativa: true,
+// };
 
 const mario = {
   cliente: 'Mario',
